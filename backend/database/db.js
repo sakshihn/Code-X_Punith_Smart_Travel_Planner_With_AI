29f821f769fe4d4900018ce8c691848af26ec9f9
SAKSHI H N <sakshinhoskeri@gmail.com>
2025-11-22 10:50:16 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const url="mongodb+srv://dineshkarthikrajand22cse:15xNZxGVX4zWzkHL@cluster0.meoth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const url="your_url";
 
 const dbconnect=async()=>{
     try {
